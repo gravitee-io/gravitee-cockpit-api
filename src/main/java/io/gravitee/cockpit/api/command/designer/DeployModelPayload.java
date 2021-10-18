c6633a50a2a599eb09e8aa98378f7b0e589ccef2
Julien Giovaresco <dev@giovaresco.fr>
2021-10-18 12:54:15 +0200

    feat: add option to publish the api on model deployment

--- src/main/java/io/gravitee/cockpit/api/command/designer/DeployModelPayload.java
+++ src/main/java/io/gravitee/cockpit/api/command/designer/DeployModelPayload.java
@@ -19,11 +19,23 @@ import io.gravitee.cockpit.api.command.Payload;
 
 public class DeployModelPayload implements Payload {
 
+  public enum DeploymentMode {
+    API_DOCUMENTED,
+    API_MOCKED,
+    API_PUBLISHED,
+  }
+
   private String modelId;
 
   private String userId;
 
   private String swaggerDefinition;
+
+  private String organizationId;
+
+  private String environmentId;
+
+  private DeploymentMode mode = DeploymentMode.API_DOCUMENTED;
 
   public String getModelId() {
     return modelId;
@@ -47,5 +59,29 @@ public class DeployModelPayload implements Payload {
 
   public void setSwaggerDefinition(String swaggerDefinition) {
     this.swaggerDefinition = swaggerDefinition;
+  }
+
+  public DeploymentMode getMode() {
+    return mode;
+  }
+
+  public void setMode(DeploymentMode mode) {
+    this.mode = mode;
+  }
+
+  public String getOrganizationId() {
+    return organizationId;
+  }
+
+  public void setOrganizationId(String organizationId) {
+    this.organizationId = organizationId;
+  }
+
+  public String getEnvironmentId() {
+    return environmentId;
+  }
+
+  public void setEnvironmentId(String environmentId) {
+    this.environmentId = environmentId;
   }
 }
