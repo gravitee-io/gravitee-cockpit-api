ea628be93ec35f62e36a7b4d259b10c237977bd9
Joseph Lomasney <josephlomasney@gmail.com>
2024-10-25 16:33:21 +0100

    feat: target token create command and reply

--- src/main/java/io/gravitee/cockpit/api/command/v1/targettoken/TargetTokenCommandPayload.java
+++ src/main/java/io/gravitee/cockpit/api/command/v1/targettoken/TargetTokenCommandPayload.java
@@ -13,33 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.gravitee.cockpit.api.command.v1;
+package io.gravitee.cockpit.api.command.v1.targettoken;
 
-/**
- * @author Guillaume LAMIRAND (guillaume.lamirand at graviteesource.com)
- * @author GraviteeSource Team
- */
-public enum CockpitCommandType {
-  BRIDGE,
-  SCORING_REQUEST,
-  SCORING_RESPONSE,
-  DEPLOY_MODEL,
-  DELETE_MEMBERSHIP,
-  DELETE_ENVIRONMENT,
-  DELETE_ORGANIZATION,
-  DISABLE_ENVIRONMENT,
-  DISABLE_ORGANIZATION,
-  ENVIRONMENT,
-  HELLO,
-  INSTALLATION,
-  MEMBERSHIP,
-  NODE,
-  NODE_HEALTHCHECK,
-  ORGANIZATION,
-  USER,
-  UNLINK_INSTALLATION,
-  V4_API,
-  SPEC_GEN_REQUEST,
-  SPEC_GEN_RESPONSE,
-  TARGET_TOKEN,
-}
+import io.gravitee.exchange.api.command.Payload;
+import lombok.Builder;
+
+@Builder
+public record TargetTokenCommandPayload(
+  String id,
+  String organizationId,
+  String environmentId
+)
+  implements Payload {}
