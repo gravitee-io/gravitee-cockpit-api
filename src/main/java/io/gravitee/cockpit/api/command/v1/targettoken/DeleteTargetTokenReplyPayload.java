cf9d14df446e0dee2dd3c21ec6b6e9c7d25afd84
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2025-09-19 10:05:50 +0200

    feat: add delete target token command

--- src/main/java/io/gravitee/cockpit/api/command/v1/targettoken/DeleteTargetTokenReplyPayload.java
+++ src/main/java/io/gravitee/cockpit/api/command/v1/targettoken/DeleteTargetTokenReplyPayload.java
@@ -13,36 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.gravitee.cockpit.api.command.v1;
+package io.gravitee.cockpit.api.command.v1.targettoken;
 
-/**
- * @author Guillaume LAMIRAND (guillaume.lamirand at graviteesource.com)
- * @author GraviteeSource Team
- */
-public enum CockpitCommandType {
-  BRIDGE,
-  SCORING_REQUEST,
-  SCORING_RESPONSE,
-  DEPLOY_MODEL,
-  DELETE_MEMBERSHIP,
-  DELETE_ENVIRONMENT,
-  DELETE_ORGANIZATION,
-  DELETE_TARGET_TOKEN,
-  DISABLE_ENVIRONMENT,
-  DISABLE_ORGANIZATION,
-  ENVIRONMENT,
-  HELLO,
-  INSTALLATION,
-  MEMBERSHIP,
-  NODE,
-  NODE_HEALTHCHECK,
-  ORGANIZATION,
-  USER,
-  UNLINK_INSTALLATION,
-  V4_API,
-  SPEC_GEN_REQUEST,
-  SPEC_GEN_RESPONSE,
-  TARGET_TOKEN,
-  EL,
-  EL_GEN_FEEDBACK,
-}
+import io.gravitee.exchange.api.command.Payload;
+
+public record DeleteTargetTokenReplyPayload() implements Payload {}
